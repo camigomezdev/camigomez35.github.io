20e82b1331f80c0417f2a8afb4f8e5cf3542bbed
Maria Camila Gómez <camigomez@experimentality.co>
2018-02-17 22:41:03 -0500

    Add new social networks

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -12,6 +12,7 @@ export class FooterComponent implements OnInit {
     facebook: 'https://www.facebook.com/mcgomez319',
     linkedin: 'https://www.linkedin.com/in/camigomez35',
     github: 'https://github.com/camigomez35',
+    instagram: 'https://www.instagram.com/camigomez35/',
     email: 'camigomez35@gmail.com',
     corpEmail: 'camigomez@experimentality.co'
   }
