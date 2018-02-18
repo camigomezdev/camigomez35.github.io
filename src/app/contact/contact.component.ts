27e2f89cf92755b8d25eb38ebc367e02bbca450f
Maria Camila Gómez <camigomez@experimentality.co>
2018-02-17 23:03:14 -0500

    update Contact me

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,10 +9,17 @@ export class ContactComponent implements OnInit {
 
   city = 'Medellín'
   country = 'Colombia'
-  email = 'camigomez35@gmail.com';
-  skype = '@camigomez318';
-  github = '@camigomez35';
-  linkedin = 'https://www.linkedin.com/in/camigomez35/';
+
+  personalLinks = {
+    twitter: 'https://www.twitter.com/camigomez318',
+    facebook: 'https://www.facebook.com/mcgomez319',
+    linkedin: 'https://www.linkedin.com/in/camigomez35',
+    github: 'https://github.com/camigomez35',
+    instagram: 'https://www.instagram.com/camigomez35/',
+    email: 'camigomez35@gmail.com',
+    corpEmail: 'camigomez@experimentality.co'
+  }
+
   constructor() { }
 
   ngOnInit() {
